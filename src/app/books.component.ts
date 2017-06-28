7a6061dee8ee1a04dc268d3928625c2fff91b77b
Fengwei Zhang <jerry.fengwei@gmail.com>
2017-06-28 00:08:18 -0400

    Tamplate setup

--- src/app/books.component.ts
+++ src/app/books.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class Books {
-  title = 'books';
+    books = [{'name':'Windstorm','src':'/assets/1.jpg', "rating":"5"},{'name':'Bombasto','src':'2.jpg', "rating":"2"},{'name':'Magneta','src':'3.jpg', "rating":"4"},{'name':'Tornado','src':'4.jpg', "rating":"4"}];
+
+    book = this.books[0];
+
+    constructor() {
+    this.book = {'name':'Default','src':'0', "rating":"5"};
+  }
+ 
 }
+
